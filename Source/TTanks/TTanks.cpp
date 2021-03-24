f36ea1df929c56f912ef0508bf6787431add03c4
GolubtsovMaxim <maxim.alexandrovich.0409@gmail.com>
2021-03-24 17:25:27 +0300

    first commit

---
+++ Source/TTanks/TTanks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TTanks, "TTanks" );
---
+++ Source/TTanks/TTanksGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TTanksGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TTANKS_API ATTanksGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
